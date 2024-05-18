83882f3d65b8fd9de23c1a00feea0959d3f1b6ec
leosalgado <leonardosalgado@protonmail.com>
2024-05-17 21:20:10 -0300

    API Node

---
+++ db.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import postgres from 'postgres'
+
+// process.env
+
+// const postgres = require('postgres');
+// require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
